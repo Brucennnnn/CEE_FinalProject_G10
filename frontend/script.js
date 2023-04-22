5eb4c58d80fc81814bddfe5eed12b95ea3104f13
markthitrin <thitrinwork@gmail.com>
2023-04-23 02:02:15 +0700

    create DealineBox, taskitem, duntion that automatically create a taskitem

--- frontend/script.js
+++ frontend/script.js
@@ -1,44 +1,16 @@
 function addTask() {
-    const taskInput = document.getElementById('taskInput');
-    const taskText = taskInput.value.trim();
-  
-    if (taskText === '') {
-      alert('Please enter a task.');
-      return;
-    }
-  
-    const taskList = document.getElementById('taskList');
-    const newTask = document.createElement('div');
-    newTask.className='list-item'
-    const la=document.createElement('label');
-    la.className='checkbox';
-    const inp =document.createElement('input');
-    inp.className='checkbox_input';
-    inp.type='checkbox';
-    inp.id=`${generateRandomId(10)}`;
-    inp.setAttribute("onchange","handleCheckboxChange(this)");
+  const taskInput = document.getElementById('taskInput');
+  const taskText = taskInput.value.trim();
 
-
-    const di = document.createElement('div');
-    di.className='checkbox_box';
-    
-    la.appendChild(inp);
-    la.appendChild(di);
-    newTask.appendChild(la);
-    const taskSpan = document.createElement('span');
-    taskSpan.textContent = taskText;
-    newTask.appendChild(taskSpan);
-    
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.onclick = function() {
-      taskList.removeChild(newTask);
-    };
-  
-    newTask.appendChild(deleteButton);
-    taskList.appendChild(newTask);
-    taskInput.value = '';
+  if (taskText === '') {
+    alert('Please enter a task.');
+    return;
   }
+
+  const taskList = document.getElementById('taskList');
+  taskList.appendChild(getTaskItem(taskInput.value,new Date(2342342)));
+  taskInput.value = '';
+}
 function generateRandomId(length) {
   var result = '';
   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -54,4 +26,70 @@ function handleCheckboxChange(checkbox){
   if(checkbox.checked){
     console.log(checkbox.id)//change to add in below list
   }
+}
+
+function getDeadlineBox(now,outdate) {
+  const hour = outdate.getHours();
+  const minute = outdate.getMinutes();
+  let time_string = hour.toString() + ":" + minute.toString();
+  time_string = hour <= 9 ? "0" + time_string : time_string;
+  let object = document.createElement("div");
+
+  let top = document.createElement("div");
+  top.className = "outdate_top";
+  if(now < outdate) {
+    top.style.background = "#D43F00";
+  }
+  else {
+    top.style.background = "#1ED400";
+  }
+
+  let bottom = document.createElement("div");
+  bottom.className = "outdate_bottom";
+  bottom.textContent = time_string;
+
+  object.appendChild(top);
+  object.appendChild(bottom);
+  object.className = "outdate";
+  return object;
+}
+
+function getCheckBox() {
+    const la=document.createElement('label');
+    la.className='checkbox';
+    const inp =document.createElement('input');
+    inp.className='checkbox_input';
+    inp.type='checkbox';
+    inp.id=`${generateRandomId(10)}`;
+    inp.setAttribute("onchange","handleCheckboxChange(this)");
+
+
+    const di = document.createElement('div');
+    di.className='checkbox_box';
+    
+    la.appendChild(inp);
+    la.appendChild(di);
+    return la
+}
+
+function getTaskItem(text, outdate) {
+  if(text.length > 100) {
+    text = text.slice(0,100) + "...";
+  }
+  let taskitem = document.createElement("div");
+  taskitem.className = "taskitem"
+
+  let front = document.createElement("div");
+  front.style.display = "flex";
+  front.appendChild(getCheckBox());
+  let front_text = document.createElement("div");
+  front_text.style = "margin-left: 3%; word-wrap: break-word; width: 21vw;";
+  front_text.textContent = text;
+  front.appendChild(front_text);
+
+  let back = getDeadlineBox(Date.now(), outdate);
+
+  taskitem.appendChild(front);
+  taskitem.appendChild(back);
+  return taskitem;
 }
