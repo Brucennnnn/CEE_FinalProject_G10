9d491667fb03dd4cca61f98bbffc57ed16bba953
Wasrek <w.wichada26@gmail.com>
2023-04-24 01:30:13 +0700

    update all finish

---
+++ frontend/Calendar/Calendar.js
@@ -0,0 +1,106 @@
+let calendar = document.querySelector('.calendar')
+
+const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+isLeapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 ===0)
+}
+
+getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28
+}
+
+function changeSelect(e){
+    // const last = document.querySelector('.curr-date');
+    // last.classList.remove('curr-date');
+    const last = document.querySelector('.select-date');
+    if(last != null) last.classList.remove('select-date');
+
+    e.currentTarget.classList.add('select-date');
+    console.log(this.innerHTML.substring(0, this.innerHTML.indexOf('<')))
+    console.log(curr_month.value+1)
+    console.log(curr_year.value)
+}
+
+
+generateCalendar = (month, year) => {
+
+    let calendar_days = calendar.querySelector('.calendar-days')
+    let calendar_header_year = calendar.querySelector('#year')
+
+    let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    calendar_days.innerHTML = ''
+
+    let currDate = new Date()
+    if (month > 11 || month < 0) month = currDate.getMonth()
+    if (!year) year = currDate.getFullYear()
+
+    let curr_month = `${month_names[month]}`
+    month_picker.innerHTML = curr_month
+    calendar_header_year.innerHTML = year
+
+    // get first day of month
+    
+    let first_day = new Date(year, month, 1)
+
+    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+        let day = document.createElement('div')
+        if (i >= first_day.getDay()) {
+            day.classList.add('calendar-day-hover')
+            day.innerHTML = i - first_day.getDay() + 1
+            day.innerHTML += `<span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>`
+            if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
+                day.classList.add('curr-date')
+            }
+        }
+        day.addEventListener('click', changeSelect);
+        calendar_days.appendChild(day)
+    }
+    // var days = document.getElementsByClassName("calendar-days");
+    // console.log(calendar_days.length);
+    // for (var i = 0; i < calendar_days.length; i++) {
+    //     console.log(calendar_days.length);
+    //     console.log(calendar_days[i])
+    //     calendar_days[i].addEventListener('click', changeSelect);
+    // }
+}
+
+let month_list = calendar.querySelector('.month-list')
+
+month_names.forEach((e, index) => {
+    let month = document.createElement('div')
+    month.innerHTML = `<div data-month="${index}">${e}</div>`
+    month.querySelector('div').onclick = () => {
+        month_list.classList.remove('show')
+        curr_month.value = index
+        generateCalendar(index, curr_year.value)
+    }
+    month_list.appendChild(month)
+})
+
+let month_picker = calendar.querySelector('#month-picker')
+
+month_picker.onclick = () => {
+    month_list.classList.add('show')
+}
+
+let currDate = new Date()
+
+let curr_month = {value: currDate.getMonth()}
+let curr_year = {value: currDate.getFullYear()}
+
+generateCalendar(curr_month.value, curr_year.value)
+
+document.querySelector('#prev-year').onclick = () => {
+    --curr_year.value
+    generateCalendar(curr_month.value, curr_year.value)
+}
+
+document.querySelector('#next-year').onclick = () => {
+    ++curr_year.value
+    generateCalendar(curr_month.value, curr_year.value)
+}
---
+++ frontend/Completed/Completed.js
@@ -0,0 +1,36 @@
+const button = document.getElementById("myButton");
+const circle = document.getElementById("circle");
+const circleNumber = document.getElementById("circle-number");
+const text = document.getElementById("text");
+let numberTask = 0;
+
+const c1 = "white";
+const c2 = "#595858";
+
+// circle.appendChild(circleNumber);
+// circleNumber.innerHTML = numberTask;
+
+// button.appendChild(circle);
+// button.appendChild(text);
+
+function changeColor() {
+    button.classList.toggle("clicked");
+    circle.classList.toggle("clicked");
+    if (circle.style.backgroundColor === c1) {
+      circle.style.backgroundColor = c2;
+      button.style.backgroundColor = c1;
+      text.style.color = "Black";
+    } else {
+      circle.style.backgroundColor = c1;
+      button.style.backgroundColor = c2;
+      text.style.color = c1;
+    }
+}
+
+function PageChange() {
+  // circleNumber.innerHTML = numberTask;
+  console.log("Hi");
+}
+
+button.addEventListener("click", changeColor);
+button.addEventListener("click", PageChange);
